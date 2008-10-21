3f3b5275bbe0ff8526a643a8d26cf32b58f56233
Yusuke Ohmichi(maimuzo) <maimuzo@gmail.com>
2008-10-21 12:48:18 +0900

    added excepton_notifier

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,6 +7,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include ApplicationHelper
+  include ExceptionNotifiable
+
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
